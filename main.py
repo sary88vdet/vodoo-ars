654a197614fadafbfa1e0c408910b752f09adf1f
Rye Salvador <salvadorrye@gmail.com>
2023-10-28 17:39:50 -0700

    Let the user choose between AR92 or RE11 report

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+# Vodoo ARS (VDET Odoo Automated Reports Software)
+# By Sary 2023 - sary88vdet@gmail.com
+
 import xmlrpc.client
 from getpass import getpass
-from sagu.dataex import save_to_csv, generate_report, generate_ar93, save_to_csv_ar93
 from datetime import date
+import sys
 
 # Enter Odoo server information
 url = input('Enter URL: ')
 db = input('Enter database: ')
-#year = input('What year to extract from? ')
 username = input('Username: ')
 password = getpass()
+# Prompt the user for a choice
+user_choice = input("Enter 'A' to generate AR92 or 'B' to generate RE11: ")
 
 # Create a dictionary to store Down Payment references
 down_payment_references = {}
@@ -17,10 +21,15 @@ down_payment_references = {}
 date_today = date.today().strftime("%Y.%m.%d")
 monthly_payments_csv_filename = f'monthly_payments_{date_today}.csv'
 monthly_invoices_csv_filename = f'monthly_invoices_{date_today}.csv' 
+re11_csv_filename = f'vdet_re11_{date_today}'
 
 # Authenticate and obtain the user's session ID
-common = xmlrpc.client.ServerProxy(f'{url}/xmlrpc/2/common')
-uid = common.authenticate(db, username, password, {})
+try:
+    common = xmlrpc.client.ServerProxy(f'{url}/xmlrpc/2/common')
+    uid = common.authenticate(db, username, password, {})
+except:
+    print(f'Exception Error Occurred')
+    sys.exit(1)        
 
 # Create a new client object for the authenticated user
 models = xmlrpc.client.ServerProxy(f'{url}/xmlrpc/2/object')
@@ -30,7 +39,12 @@ model = 'account.payment' #model name for Customer Payments
 fields = ['id', 'date', 'amount', 'payment_method_line_id', 'name', 'move_id', 'partner_id', 'state']
 
 # Search for payments (you can add filters if needed)
-payment_ids = models.execute_kw(db, uid, password, model, 'search', [[]])
+try:
+    payment_ids = models.execute_kw(db, uid, password, model, 'search', [[]])
+except xmlrpc.client.Fault as error:
+    print(f'Error: {error}')
+    sys.exit(1)
+
 payments = models.execute_kw(db, uid, password, model, 'read', [payment_ids], {'fields': fields})
 
 model = 'account.move.line' #model name for General Ledger
@@ -55,13 +69,24 @@ for ledger_row in general_ledger:
     if ledger_row['name'] == 'Down Payment':
         down_payment_references[ledger_row['partner_id'][-1]] = ledger_row['credit']
 
-#print('\nGenerating monthly payment sums...')
-#monthly_payment_sums = generate_report(payments, down_payment_references)
-#save_to_csv(monthly_payments_csv_filename, monthly_payment_sums)
-monthly_payment_sums = generate_ar93(payments, down_payment_references)
+# Check the user's choice
+if user_choice == 'A':
+    print("Generating report for AR92")
+    from sagu.ar92 import save_to_csv, generate_report
+elif user_choice == 'B':
+    print("Generating report for RE11")
+    from sagu.re11 import save_to_csv, generate_report
+else:
+    print("Invalid input. Please enter 'A' or 'B'.")
 
-#print('\nGenerating monthly invoice sums...')
-#monthly_invoice_sums = generate_report(invoices, down_payment_references, 'invoice_date', 'amount_total')
+print('\nGenerating monthly payment sums...')
+monthly_payment_sums = generate_report(payments, down_payment_references)
 
-monthly_invoice_sums = generate_ar93(invoices, down_payment_references, 'invoice_date', 'amount_total')
-save_to_csv_ar93(monthly_invoices_csv_filename, monthly_invoice_sums, monthly_payment_sums)
+print('\nGenerating monthly invoice sums...')
+monthly_invoice_sums = generate_report(invoices, down_payment_references, 'invoice_date', 'amount_total')
+
+if user_choice == 'A':
+    save_to_csv(monthly_payments_csv_filename, monthly_payment_sums)
+    #save_to_csv(monthly_invoices_csv_filename, monthly_invoice_sums)
+elif user_choice == 'B':
+    save_to_csv(re11_csv_filename, monthly_invoice_sums, monthly_payment_sums)
--- sagu/re11.py
+++ sagu/re11.py
@@ -1,20 +1,14 @@
+# Vodoo ARS (VDET Odoo Automated Reports Software)
+# By Sary 2023 - sary88vdet@gmail.com
+
 import csv
 from datetime import datetime
 import calendar
 
-def save_to_csv(base_filename, year_sums):
-    for year, month_sums in year_sums.items():
-        filename = f'{base_filename}_{year}.csv'
-        with open(filename, 'w', newline='') as outfile:
-            writer = csv.writer(outfile)
-            writer.writerow(['Month'] + list(month_sums.keys()))
-            writer.writerow(['Total Amount'] + list(month_sums.values()))
-        print(f'Finished writing AR92 CSV report for {year} to {filename}.')
-
-def save_to_csv_ar93(base_filename, invoice_sums, payment_sums):
+def save_to_csv(base_filename, invoice_sums, payment_sums):
     # First extract key and value from invoice
     for year, customer in invoice_sums.items():
-        # It's a dictionary within a dictionary
+        # invoice_sums and payment_sums are a dictionary within a dictionary
         print(f'Printing customer {customer}')
         first_iteration = True
         for value in customer.values():
@@ -25,12 +19,12 @@ def save_to_csv_ar93(base_filename, invoice_sums, payment_sums):
         filename = f'{base_filename}_{year}.csv'
         with open(filename, 'w', newline='') as outfile:
             writer = csv.writer(outfile)
-            writer.writerow(['Contract', 'Address'] + [key for key in keys for _ in range(2)])
-            
+            writer.writerow(['Contract', 'Address'] + [key + ' ' + str(year) for key in keys for _ in range(2)])
+            writer.writerow(['', ''] + ['Expected', 'Real'] * 12)
             for key, value in customer.items():
                 row = []
-                row.append(key)
-                row.append('placeholder')
+                row.append(key.split(' ')[0]) #contract
+                row.append('') #placeholder for customer address
                 for month in keys:
                     print(f'{month}/{year}')
                     print(f'Invoice:: {customer[key][month]}')
@@ -45,52 +39,7 @@ def save_to_csv_ar93(base_filename, invoice_sums, payment_sums):
                 print(row)
                 writer.writerow(row)
 
-# Generate report for monthly payments
 def generate_report(records, downpayment_filter=dict(), date_key='date', amount_key='amount'):
-    # Reset values of month sums to 0
-    year_sums = {}
-
-    for record in records:
-        try:
-            record_date = datetime.strptime(record[date_key], '%Y-%m-%d')
-        except TypeError:
-            print(f'Warning: record {record["name"]} has no date. Unable to add it to the report!')
-            continue
-        
-        year = record_date.year
-        month = calendar.month_name[record_date.month] 
-        amount = float(record[amount_key])
-        
-        if year not in year_sums:
-            year_sums[year] = {
-                'January': 0,
-                'February': 0,
-                'March': 0,
-                'April': 0,
-                'May': 0,
-                'June': 0,
-                'July': 0,
-                'August': 0,
-                'September': 0,
-                'October': 0,
-                'November': 0,
-                'December': 0,
-            }
-        
-        if record['partner_id'] and record['partner_id'][-1] in downpayment_filter.keys():
-            if downpayment_filter[record['partner_id'][-1]] == amount:
-                print(f'\"{record["partner_id"][-1]}\" amount {amount} is a downpayment! Not adding in the report.')
-                continue
-            else:
-                print(f'Adding \"{record["partner_id"][-1]}\" amount {amount} to the report.')
-                year_sums[year][month] += amount
-        elif record['partner_id']:
-            print(f'Adding \"{record["partner_id"][-1]}\" amount {amount} to the report.')
-            year_sums[year][month] += amount
-
-    return year_sums
-
-def generate_ar93(records, downpayment_filter=dict(), date_key='date', amount_key='amount'):
     year_sums = {}
 
     for record in records:
@@ -131,11 +80,5 @@ def generate_ar93(records, downpayment_filter=dict(), date_key='date', amount_ke
             else:
                 print(f'Adding record {record["name"]} with amount {amount} to report.')
                 year_sums[year][partner_id][month] += amount
-    #print(year_sums)
-
-    for k, v in year_sums.items():
-        print(k)
-        if k == 2023:
-            print('\n')
-            print(year_sums[k])
+   
     return year_sums
