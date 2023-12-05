71af896eab3253a746dd7ee2acbcf559e8f9ccb2
Rye Salvador <salvadorrye@gmail.com>
2023-12-05 05:33:54 -0700

    Added new columns for total debt, debt before payment, current month debt, debt after payment, current month and payment in adv

--- main.py
+++ main.py
@@ -1,52 +1,58 @@
-# Vodoo ARS (VDET Odoo Automated Reports Software)
-# By Sary 2023 - sary88vdet@gmail.com
-
 import xmlrpc.client, sys
 from getpass import getpass
-from datetime import date
-from sagu.rexx import save_to_csv, generate_report
+from datetime import date, datetime
+from vodoo.ars import year_sums, generate_report, save_to_csv
 
-# Enter Odoo server information
 url = input('Enter URL: ')
 db = input('Enter database: ')
 username = input('Username: ')
 password = getpass()
 
-# CSV reports filename 
-date_today = date.today().strftime("%Y.%m.%d") 
-re12_csv_filename = f'vdet_re12_{date_today}'
-
 # Authenticate and obtain the user's session ID
 try:
     common = xmlrpc.client.ServerProxy(f'{url}/xmlrpc/2/common')
     uid = common.authenticate(db, username, password, {})
-except:
-    print(f'Wrong password or invalid username.')
-    sys.exit(1)        
+except xmlrpc.client.Fault as error:
+    print(f'FATAL: Database "{db}" does not exist')
+    sys.exit(1)     
 
-# Create a new client object for the authenticated user
-models = xmlrpc.client.ServerProxy(f'{url}/xmlrpc/2/object')
+# Connect to the database
+models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
 
-def get_objects(model, fields):
-    try:
-        object_ids = models.execute_kw(db, uid, password, model, 'search', [[]])
-    except xmlrpc.client.Fault as error:
-        print(f'Error: {error}')
-        sys.exit(1)
+def get_objects(model, fields, object_ids=None):
+    if not object_ids:
+        try:
+            object_ids = models.execute_kw(db, uid, password, model, 'search', [[]])
+        except xmlrpc.client.Fault as error:
+            print(f'Error: {error}')
+            sys.exit(1)
     return models.execute_kw(db, uid, password, model, 'read', [object_ids], {'fields': fields})
 
-payments = get_objects('account.payment', ['id', 'date', 'amount', 'payment_method_line_id', 'name', 'move_id', 'partner_id', 'state'])
+payments_all = get_objects('account.payment', ['id', 'date', 'amount', 'payment_method_line_id', 'name', 'move_id', 'partner_id', 'state'])
 
-invoices = get_objects('account.move', ['id', 'name', 'partner_id', 'invoice_date', 'invoice_date_due',
-        'amount_untaxed', 'amount_total', 'payment_state', 'state'])
+# Search for customers with non-empty 'invoice_ids'
+customer_model = 'res.partner'
+customers_with_invoices = models.execute_kw(db, uid, password, customer_model, 'search_read', [[['invoice_ids', '!=', False]]], {'fields': ['id', 'name', 'street', 'invoice_ids']})
 
-customers = get_objects('res.partner', ['id', 'street'])
+customers_with_data = []
+for customer in customers_with_invoices:
+    
+    invoice_ids = customer['invoice_ids']
+    invoices = get_objects('account.move', ['name', 'invoice_date', 'amount_total'], invoice_ids)
+    invoices_with_dates = [invoice for invoice in invoices if invoice['invoice_date']]
+   
+    payments = []
+    for payment in payments_all:
+        if payment['partner_id'][0] == customer['id']:
+            payments.append(payment)
 
-address_map = {}
-for customer in customers:
-    address_map[customer['id']] = customer['street']
+    yearly_invoice_sums = year_sums(invoices_with_dates, 'invoice_date', 'amount_total')
+    yearly_payment_sums = year_sums(payments)
 
-print("Generating report for RE13")
-monthly_payment_sums = generate_report(payments)
-monthly_invoice_sums = generate_report(invoices, 'invoice_date', 'amount_total')
-save_to_csv(monthly_invoice_sums, monthly_payment_sums, address_map)
+    customer['old_debts'] = generate_report(yearly_invoice_sums, yearly_payment_sums)
+    customer['expected'] = yearly_invoice_sums
+    customer['real'] = yearly_payment_sums
+    
+    customers_with_data.append(customer)
+    
+save_to_csv(customers_with_data) 
---
+++ vodoo/ars.py
@@ -0,0 +1,173 @@
+import calendar, csv
+from datetime import datetime, date
+
+def save_to_csv(customers):
+    date_today = date.today().strftime("%Y.%m.%d")
+    base_filename = input('Name to save CSV file: ') + f'_{date_today}'
+
+    months = list(calendar.month_name)[1:]
+    years = set()
+
+    for customer in customers:
+        old_debts = customer['old_debts']
+        years.update(old_debts.keys())
+
+    for year in years:    
+        months_reversed = list(reversed(months))
+
+        filename = f'{base_filename}_{year}.csv'
+        with open(filename, 'w', newline='') as outfile:
+            writer = csv.writer(outfile)
+            writer.writerow(['Contract', 'Address'] + [month + ' ' + str(year) for month in months_reversed for _ in range(8)])
+            writer.writerow(['', ''] + ['Expected', 'Real', 'Total Debt', 'Debt Before Payment', 'Current Month Debt', 'Debt After Payment', 'Current Month', 'Payment in Advance'] * 12)
+            
+            for customer in customers:
+                row = []
+                row.append(customer['name'])
+                row.append(customer['street'])
+                for month in months_reversed:
+                    
+                    row.append(customer['expected'].get(year, {}).get(month, '-'))
+                    row.append(customer['real'].get(year, {}).get(month, '-'))
+                    
+                    if year in customer['old_debts']:
+                        row.append(customer['old_debts'][year][month]['total_debt']) 
+                        row.append(customer['old_debts'][year][month]['debt_before_payment'])
+                        row.append(customer['old_debts'][year][month]['current_month_debt'])
+                        row.append(customer['old_debts'][year][month]['debt_after_payment'])
+                        row.append(customer['old_debts'][year][month]['current_month'])
+                        row.append(customer['old_debts'][year][month]['payment_in_adv'])     
+                writer.writerow(row)
+        
+def year_sums(records, date_key='date', amount_key='amount'):
+    # Reset values of month sums to 0
+    year_sums = {}
+    
+    for record in records:
+        try:
+            record_date = datetime.strptime(record[date_key], '%Y-%m-%d')
+        except TypeError:
+            print(f'Warning: record {record["name"]} has no date. Unable to add it to the report!')
+            continue
+        
+        year = record_date.year
+        month = calendar.month_name[record_date.month] 
+        amount = float(record[amount_key])
+        
+        if year not in year_sums:
+            year_sums[year] = {
+                'January': 0,
+                'February': 0,
+                'March': 0,
+                'April': 0,
+                'May': 0,
+                'June': 0,
+                'July': 0,
+                'August': 0,
+                'September': 0,
+                'October': 0,
+                'November': 0,
+                'December': 0,
+            }
+
+        year_sums[year][month] += amount
+        sorted_keys = sorted(year_sums.keys())
+        sorted_year_sums = {key: year_sums[key] for key in sorted_keys}
+
+    return sorted_year_sums
+
+def process_payment(debt_previous_month, invoice, payment, payment_in_adv=0):
+    # Initialize variables
+    total_debt = 0
+    debt_before_payment = 0
+    current_month_debt = 0
+    debt_after_payment = 0 
+    current_month = 0
+    payment_in_adv = 0
+
+    # If without debt
+    if debt_previous_month == 0:
+        total_debt = debt_previous_month
+        debt_before_payment = total_debt + invoice
+        current_month_debt = 0
+
+        if payment <= invoice:
+            debt_after_payment = invoice - payment
+            current_month = payment
+            payment_in_adv += 0
+        else:
+            payment_in_adv += payment - invoice
+            current_month = invoice
+            debt_after_payment = 0
+
+    # If with debt
+    else:
+        total_debt = debt_previous_month
+        debt_before_payment = total_debt + invoice
+
+        if payment < total_debt:
+            current_month_debt = payment
+            debt_after_payment = invoice + (total_debt - payment)
+            current_month = 0
+            payment_in_adv += 0
+        else: #payment is larger than total_debt
+            current_month_debt = total_debt
+            current_month = payment - total_debt
+            if invoice > current_month:
+                debt_after_payment = invoice - current_month
+                payment_in_adv += 0
+            elif invoice < current_month:
+                debt_after_payment = 0
+                payment_in_adv += current_month - invoice
+
+    # Return relevant values
+    return {
+        'total_debt': total_debt,
+        'debt_before_payment': debt_before_payment,
+        'current_month_debt': current_month_debt,
+        'debt_after_payment': debt_after_payment,
+        'current_month': current_month,
+        'payment_in_adv': payment_in_adv
+    }
+
+def generate_report(invoice_sums, payment_sums):
+
+    old_debts = {}
+    debt_previous_month = 0
+
+    for year, monthly_invoice_sums in invoice_sums.items():
+        if year not in old_debts:
+            old_debts[year] = {
+                'January': {},
+                'February': {},
+                'March': {},
+                'April':{},
+                'May': {},
+                'June': {},
+                'July': {},
+                'August': {},
+                'September': {},
+                'October': {},
+                'November': {},
+                'December': {},
+            }
+
+        for month, month_invoice_sum in monthly_invoice_sums.items():
+            if month_invoice_sum == 0:
+                # return all vars as 0
+                old_debts[year][month]['total_debt'] = 0
+                old_debts[year][month]['debt_before_payment'] = 0
+                old_debts[year][month]['current_month_debt'] = 0
+                old_debts[year][month]['debt_after_payment'] = 0
+                old_debts[year][month]['current_month'] = 0
+                old_debts[year][month]['payment_in_adv'] = 0
+            else:
+                old_debts[year][month] = process_payment(
+                    debt_previous_month,
+                    month_invoice_sum,
+                    payment_sums.get(year, {}).get(month, 0)
+                    )
+            # Update the debt for previous month        
+            debt_previous_month = old_debts[year][month]['debt_after_payment']        
+
+    return old_debts
